636a4abbab4a335f6c635a8e1babab8e40fac68f
tomilone <tonymilone.tech@gmail.com>
2020-06-10 17:57:25 -0800

    minor changes

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-
-export default function Card (props){
-    return (
-        
-        <>
-            <h3 className='card title'>{props.title}</h3>
-                <p className='card tries'>{props.tries} <i class="fas fa-users"></i> Tried It!</p>
-        </>
-    )
+export default function Card(props) {
+  return (
+    <>
+      <h3 className="card title">{props.title}</h3>
+      <p className="card tries">
+        {props.tries} <i className="fas fa-users"></i> Tried It!
+      </p>
+    </>
+  );
 }
--- src/Components/ExpandCard/ExpandedView.js
+++ src/Components/ExpandCard/ExpandedView.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import tryItButton from '../../logo/TryItButton.png';
 
-export default function ExpandedView(props){
-    return(
-        <div>
-            <h2>{props.title}</h2>
-            <p>{props.content}</p>
-            <button onClick={(e) => props.update(e,props.id,props.tries,props.closeModal)}>Try It!</button>
-            <p>{props.tries} Tried It!</p>
-        </div>
-    )
+export default function ExpandedView(props) {
+  return (
+    <div>
+      <h2>{props.title}</h2>
+      <p>{props.content}</p>
+      <button
+        onClick={(e) =>
+          props.update(e, props.id, props.tries, props.closeModal)
+        }
+      >
+        <img src={tryItButton} alt="Try It Button" />
+      </button>
+      <p>
+        {props.tries} <i className="fas fa-users"></i> Tried It!
+      </p>
+    </div>
+  );
 }
